328d5ed3d73260bc046e417fff69a8e9f48cb115
LukeF-stack <luke.fordham01@gmail.com>
2021-02-09 11:16:07 +0800

    added code comments to Filter component

--- React/src/components/Filter.tsx
+++ React/src/components/Filter.tsx
@@ -11,14 +11,18 @@ declare module 'react' {
 
 }
 
+// Filter component
 const Filter: React.FC = () => {
 
+  // use-between functions destructuring
   const { formModel, add }: any = useSharedForm();
   const { formView, setView }: any = useSharedForm();
   const { formEls, addEls }: any = useSharedFormEls();
-
+  
+  // Create task html elements from todo list as (data)
   function createEls(data: any, identifier: any) {
     const todoEls: any = [];
+    // create html element for each todo if the item isn't undefined
     data.forEach((item: any) => {
       if (item !== undefined) {
         todoEls.push(
@@ -32,107 +36,171 @@ const Filter: React.FC = () => {
     return todoEls;
   }
 
+// filter the tasks and reset the view state -- key: keyword (searchbar) / user Id (dropdown filter) -- input: defines type of search
 const filterTodos = (key: any, input: any) => {
+  // declare result variable
   let result: any = '';
   const formData: any = [];
+  // if the input is id (user dropdown)
   if (input === 'id'){
+    // set result to todos that either match the key user id OR the array of user ids includes the todo.user id
     result = formModel.todos.filter((obj: any) => obj.user == key || key.includes(obj.user));
+    // set the users in formdata to include only the user selected in the user dropdown
     formData['users'] = formModel.users.filter((obj: any) => obj.id == key || key.includes(obj.id));
   } 
+  // if the input is a keyword (searchbar)
   if (input ==='keyword') {
+    // filter the users in the model state by getIds('view'): (gets all users in the view state) and set 'todos' variable to all the todos relevent to that todo.user id
     const todos = formModel.todos.filter((obj: any) => getIds('view').includes(obj.user));
+    // set result to all the todos (already filtered by user dropdown) that include the search keyword/letters
     result =todos.filter((obj: any) => obj.name.toLowerCase().includes(key));
+    // if the view already has users
     if (formView.users) {
+      // set the users in the formdata to the users in the view state
       formData['users'] = formView.users;
-    } else {
+    } 
+    // else set the users in the formData to all the users in the model state
+    else {
       formData['users'] = formModel.users;
     }
   } 
+  // set the todos in the formData to the filtered todos 
   formData['todos'] = result;
-  console.log('result is', result);
+  // console.log('result is', result);
+
+  // set the view state to the formData array
   setView(formData)
 }
 
+
+// get the name of a user by user id
 const findUser = (key: any) => {
   const result = formModel.users.find((obj: any) => obj.id == key);
+  // return a string of the user's first and last name
   return result.firstName + ' ' + result.lastName;
 }
 
+// runs when the component mounts
   useEffect(()=> {
     const formData: any = [];
+    // fetchData function
     const fetchData: VoidFunction = async () => {
+      // get all the users from the server
         try{
             const response = await fetch('api/users');
             const users = await response.json();
+            // set users i the formData array to the returned users
             formData['users'] = users.users;
-        } catch (e) {
+        } 
+        // catch errors
+        catch (e) {
             console.log(e);
         }
+        // get all the todos from the server
         try{
             const response = await fetch('api/todos');
             const todos = await response.json();
+            // set the todos in the formData array to the returned todos
             formData['todos'] = todos.todos;
-        } catch (e) {
+        } 
+        // catch errors
+        catch (e) {
             console.log(e);
         }
+        // set the model state to the formData (returned from the fetch requests)
         add(formData);
     }
+    // run the fetchdata function
     fetchData();
 }, []);
 
-
+// run when the view state changes
   useEffect(() => {
+    // log the view state
       console.log('view is', formView);
+      // log that the state has changed
       console.log('changed')
+      // if there are todos in the view state
       if (formView.todos) {
         const updateEls: any = [];
+        // set todoEls to html elements returned from createEls() function
         const todoEls = createEls(formView.todos, 'todo');
+        // set todos in updateEls array to the created todo elements
         updateEls['todos'] = todoEls;
+        // if there are users in the formEls state 
         if (formEls.users) {
+          // set the users in the updateEls array to the users in the formEls state
           updateEls['users'] = formEls.users;
         }
+        // set the formEls state to the updateEls array
         addEls(updateEls);
       }
   }, [formView])
 
+
+  // run when the model state changes
   useEffect(() => {
+    // set the view state to the model state
     setView(formModel);
+    // if the model state contains users
     if (formModel.users) {
       const userDropdown: any = [];
+      // push an 'all users' option to the user dropdown array
+      userDropdown.push({"value": getIds('all'), "label": "All Users"})
+      // for each user in the model state
       formModel.users.forEach((user: any) => {
+        // push an option for the user to the user dropdown array
         userDropdown.push(
           {"value": user.id, "label": user.firstName}
         )
       })
-      userDropdown.push({"value": getIds('all'), "label": "All Users"})
       const updateEls: any = [];
+      // set the users in the updateEls array to the userDropdown array
       updateEls['users'] = userDropdown;
+      // if there are todos in the formEls state
       if (formEls.todos) {
+        // set the todos in the updateEls array to the todos in the formEls state
         updateEls['todos'] = formEls.todos;
       }
+      // set the formEls to the updateEls array
       addEls(updateEls);
     }
   }, [formModel])
 
+
+  // run when the formEls state changes
   useEffect(() => {
+    // console log the formEls state
     console.log('formEls:', formEls);
   }, [formEls])
 
+
+  // get user ids -- type: get the users in the model or the view state
   function getIds(type: any){
     const ids: any = []; 
     let form: any = '';
+    // if the type is all
     if (type === "all"){
+      // set the form variable to the users in the model state
       form = formModel.users
-    } else if (type ==="view"){
+    } 
+    // otherwise if the type is view
+    else if (type ==="view"){
+      // set the form variable to te users in the view state
       form = formView.users
     }
+    // for each user in the form variable
     form.forEach((user: any) => 
+    // push the user id to the ids array
     ids.push(user.id))
+    // return the ids array
     return ids;
   }
 
+
+  // handles the search
   function handleSearch(value: any) {
-    //console.log(formView.users);
+    // run the filterTodos function with the searchbar input value and the 'keyword' filter type
     filterTodos(value, 'keyword')
   }
 
@@ -143,12 +211,6 @@ return (
       <TextField label="Search Tasks" onKeyUp={(e: any) => {handleSearch(e.target.value)}} />
       {formEls.users ? <Select placeholder="User" 
       options={formEls.users} onChange={(e: any) => {filterTodos(e.value, 'id')}} /> : null}
-      {/* {formEls.users ? 
-      <select onChange={(e) => {filterTodos(e.target.value)}} name="users" id="select_users">
-        <option value="" disabled selected hidden>User</option>
-        <option value={getIds()} >All users</option>
-        {formEls.users}
-      </select> : null} */}
   </div>
 );
 }
