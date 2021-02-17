05ee627cbd8f3310052c1893b44ea9cc3f4805d6
LukeF-stack <luke.fordham01@gmail.com>
2021-02-17 09:42:09 +0800

    can only update todo name if not empty

--- React/src/components/updateTodo.ts
+++ React/src/components/updateTodo.ts
@@ -1,21 +1,25 @@
 
     const updateTodo = async (todo: any) => {
-        try{
-            const settings = {
-            method: "PUT",
-            body: JSON.stringify(todo),
-            params: {"id": todo.id}
-            };
-
-            const response = await fetch(`api/todo/${todo.id}/update`, settings);
-            const status = await response.json();
-            console.log('status of put request:', status)
-            const success: any = {"status": true, "newTodo": status.newTodo};
-            return success;
-        } 
-        // catch errors
-        catch (e) {
-            console.log(e);
+        if (todo.name !== (null || '')) {
+            try{
+                const settings = {
+                method: "PUT",
+                body: JSON.stringify(todo),
+                params: {"id": todo.id}
+                };
+    
+                const response = await fetch(`api/todo/${todo.id}/update`, settings);
+                const status = await response.json();
+                console.log('status of put request:', status)
+                const success: any = {"status": true, "newTodo": status.newTodo};
+                return success;
+            } 
+            // catch errors
+            catch (e) {
+                console.log(e);
+                return false;
+            }
+        } else {
             return false;
         }
     }
